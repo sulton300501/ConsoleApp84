cb52feae9d7709cc9ba1e05bfb31d0c21f6e7f99
sulton <global-sultonmalik326@gmail.com>
2024-02-06 17:24:59 +0500

    Add project files.

---
+++ ConsoleApp84/Program.cs
@@ -0,0 +1,20 @@
+﻿/*     birinchi mavzu tanlanadi. va 
+
+    bitta vaqtni belgilap har kuni shu vaqt oraligida oqish kerak boladi . odam oziga qiziq bolgan kitobni oqishi kerak boshqa kitoblar u 
+    uchun qiziq bomiydi va eslap qolishga ham harakat qilmiydi  . hamma uchun kitob oqish har xil bolishi mumkin kimdir oxirigacha bir kunda 
+    oqiydi kimlardir haftalarga bo'lip oqiydi hamma ozi uchun  qulay usulni tanlashi kerak. kitob oqish jarayonida chalgituvchi narsalardan 
+    uzoq turip diqqatni jamlashi shart. Bazida guruh bilan oqish yaxshi samara berishi mumkin. Dasturlarni o'rganish menda qanaqa boladi hech 
+    qachon birinchi ko'rgan dasturim yoki o'rganayotgan tilim bir qarashda aniq tushunolmiyman , 2   yoki  3 marta boshqa manbalardan foydalanganimda 
+    ozgina tushunarli boladi asosiy effect  4 kunda sezila boshliydi va bu darsga yana qaytishimga togri keladi.
+    Nma qilsam  haqiqiy programist bolaman --  endi dasturlashga kirip kelgan bola nazariy bilan koproq shugullanishi kerak biroz nazariy ilmi
+    bo'lgandan keyin faqat proyektlar bilan shugullanishi 
+    va atrofiga kuchli dasturchilarni yigishi ulardan koproq narsani o'rganishga harakat qilishi kerak*/
+
+
+
+/*Katta jadvallarni qismlarga bolish ular bilan ishlash tezligini oshirip beradi..
+    Partitionni afzalligi malumotlarni tez olish, tezroq yuklash , bir vaqtning ozida 
+    bir nechta bo'limlarga malumotlarni yuklash ,
+    malumotlarni boshqarish yani katta jadvallarni  junatish o'rniga  kichiroq
+    bo'limni junatish mumkin va kamroq foydalaniladigan malumotlarni bo'limga 
+    ajratip arhivlash mumkin bu izlash samaradorligini oshirip beradi..*/
